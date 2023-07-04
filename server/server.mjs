5c8c7d5d9097efadbbcb854ff79d92a7d804e7eb
JAston <jacob.aston1@gmail.com>
2023-07-04 15:37:01 -0600

    fixed port consolelog and formatted files.

--- server/server.mjs
+++ server/server.mjs
@@ -10,5 +10,5 @@ app.use("/record", records);
 
 //start the Express server
 app.listen(PORT, () => {
-  console.log("Server is running on port: ${PORT}");
+  console.log(`Server is running on port: ${PORT}`);
 });
