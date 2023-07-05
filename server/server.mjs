b922cb839a82da7598efe15e7ee2770cbd8d3b8a
JAston <jacob.aston1@gmail.com>
2023-07-04 22:35:30 -0600

    Sample app runs locally.

--- server/server.mjs
+++ server/server.mjs
@@ -6,6 +6,9 @@ import records from "./routes/record.mjs";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.use("/record", records);
 
 //start the Express server
